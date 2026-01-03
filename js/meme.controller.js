b5f75bda472e9bd666fc261e2735617e07bf31d8
Amirvil <amir.vilensky@gmail.com>
2026-01-03 10:54:00 +0200

    Add renderMeme function

--- js/meme.controller.js
+++ js/meme.controller.js
@@ -1,1 +1,33 @@
 'use strict'
+
+var gElCanvas
+var gCtx
+
+function onInit() {
+    gElCanvas = document.querySelector('canvas')
+    gCtx = gElCanvas.getContext('2d')
+
+    renderMeme()
+}
+
+
+function renderMeme() {
+    const elImg = new Image()
+    elImg.src = 'images/2.jpg'
+
+    elImg.onload = () => {
+        gCtx.drawImage(elImg, 0, 0, elImg.naturalWidth, elImg.naturalHeight)
+        drawTxt()
+    }
+}
+
+function drawTxt() {
+    gCtx.lineWidth = 2
+    gCtx.strokeStyle = 'black'
+    gCtx.fillStyle = 'white'
+    gCtx.textAlign = 'center'
+    gCtx.font = '40px Impact'
+
+    gCtx.fillText("First Meme", 150, 50)
+    gCtx.strokeText("First Meme", 150, 50)
+}
